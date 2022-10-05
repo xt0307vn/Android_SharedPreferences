9119b4dc60c48cfd19cd6af810817bccd0b1fdc9
Xuân Trường <xt0307vn@gmail.com>
2022-10-05 14:36:08 +0700

    day 05/10/22 02:36pm

---
+++ app/src/main/java/com/example/android_sharedpreferences/GamePointActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GamePointActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gamepoint);
+    }
+}
